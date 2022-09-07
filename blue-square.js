b4ff8172c1264f51b2b2c73c1b1232f02a12210a
ExpertZs <zs.freelancer1@gmail.com>
2022-09-07 16:52:12 +0600

    Get some mistake and solved with last review
    I made some mistake when writing the code , in this phase I have recheck and Its all set to go

--- blue-square.js
+++ blue-square.js
@@ -1,45 +1,48 @@
+
 //getting the blue-square box with it's Id
 const blue_square = document.getElementById('blue-square');
 
 //initializing primeary variable
-const height =screen.height-100;
-const width = screen.width-100;
+const height =screen.height -100;
+
+const width = screen.width -100;
+
 let topValue= 50; //At the starting box is set 50px from top
 let leftValue= 0;
 let roundTop = 0;
 let roundLeft = 0;
 
 //Function for increasing or decreasing height and width value
-const findValue= (value)=>{
-    if(value =='top'){
-        if(value >=height){
-           roundTop++;
+const findValue =(value) =>{
+    if(value == 'top'){
+        if(topValue >= height){
+            roundTop++;
         }
-        if(roundTop % 2 ==0){
-            topValue += 10;
+        if(roundTop % 2 == 0){
+                topValue += 10;
         }
         else{
             topValue -= 10;
         }
-        if(topValue <=0){
-            roundTop ++;
+        if(topValue <= 0){
+            roundTop++;
         }
         return topValue;
     }
     else{
-        if(leftValue >=width){
-            roundLeft ++;
+        if(leftValue >= width){
+            roundLeft++;
         }
         if(roundLeft % 2 == 0){
-            leftValue += 10;
+                leftValue += 10;
         }
         else{
-            leftValue -=10;
+            leftValue -= 10;
         }
-        if(leftValue <=0){
-            roundLeft ++;
+        if(leftValue <= 0){
+            roundLeft++;
         }
-        return leftValue;  
+        return leftValue;
     }
 }
 
@@ -50,7 +53,7 @@ const moveBoxAnimation = ()=>{
 
     //set box position
     blue_square.style.top = `${topValue}px`;
-    blue_square.left= `${leftValue}px`;
+    blue_square.style.left= `${leftValue}px`;
 }
 
 //Calling the animation function in every second
