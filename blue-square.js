e9d89308df32d568fe55cb24620f8b47b7c3ca5d
ExpertZs <zs.freelancer1@gmail.com>
2022-09-07 15:49:26 +0600

    Initializing primary variable in javascripts
    In this section I have initialize all required primary variable in javascripts

--- blue-square.js
+++ blue-square.js
@@ -1,2 +1,10 @@
 //getting the blue-square box with it's Id
 const blue_square = document.getElementById('blue-square');
+
+//initializing primeary variable
+const height =screen.height-100;
+const width = screen.width-100;
+let topValue= 50;
+let leftValue= 0;
+let roundTop = 0;
+let roundLeft = 0;
