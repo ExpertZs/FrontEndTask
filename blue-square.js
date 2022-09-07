2ab5e7f1be6acb6729fabbf530a7a8b2ba150f9f
ExpertZs <zs.freelancer1@gmail.com>
2022-09-07 16:08:46 +0600

    Create function for increasing or decreasing  height and width value
    In this phase I have create function to increase or decrease height and width value

--- blue-square.js
+++ blue-square.js
@@ -4,7 +4,41 @@ const blue_square = document.getElementById('blue-square');
 //initializing primeary variable
 const height =screen.height-100;
 const width = screen.width-100;
-let topValue= 50;
+let topValue= 50; //At the starting box is set 50px from top
 let leftValue= 0;
 let roundTop = 0;
 let roundLeft = 0;
+
+//Function for increasing or decreasing height and width value
+const findValue= (value)=>{
+    if(value =='top'){
+        if(value >=height){
+           roundTop++;
+        }
+        if(roundTop % 2 ==0){
+            topValue += 10;
+        }
+        else{
+            topValue -= 10;
+        }
+        if(topValue <=0){
+            roundTop ++;
+        }
+        return topValue;
+    }
+    else{
+        if(leftValue >=width){
+            roundLeft ++;
+        }
+        if(roundLeft % 2 == 0){
+            leftValue += 10;
+        }
+        else{
+            leftValue -=10;
+        }
+        if(leftValue <=0){
+            roundLeft ++;
+        }
+        return leftValue;  
+    }
+}
